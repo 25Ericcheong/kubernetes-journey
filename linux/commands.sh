2671085c1be1c12d5f1878ee08dcf61a54b7ac81
25Ericcheong <ericcheongkaikit@gmail.com>
2025-11-23 11:37:49 +0800

    task: redirect content read from another file and create a new file with content read previously

--- linux/commands.sh
+++ linux/commands.sh
@@ -1,2 +1,5 @@
 # Appends to existing text (if only > will replace existing file with content)
 echo new line hello >> test-echo.txt
+
+# Redirect content from file and process with cat and then create a new file with content processed
+cat < test-echo.txt > new-test-echo.txt
