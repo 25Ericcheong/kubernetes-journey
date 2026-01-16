256a16d8ae974a363143267007ef4359a7676da2
Eric <55186326+25Ericcheong@users.noreply.github.com>
2026-01-16 18:30:18 +0800

    Update general.sh

--- kubernetes/general.sh
+++ kubernetes/general.sh
@@ -7,3 +7,6 @@ kubectl describe pod podname
 kubectl get pods -n namespace_name
 
 kubectl exec -it pod_name -- sh # go into pod and interact with shell
+
+watch -n 0.1 kubectl get pods -o wide # prints every 0.1s of all pods
+
